7fa702175fffa8e0690bd1c9eec7c6e14eba39a8
cardtuc <cardtuc@gmail.com>
2023-09-27 15:18:41 +0300

    Initial commit

---
+++ src/redux/index.ts
@@ -0,0 +1,8 @@
+import { store } from "./store";
+
+export * from "./store";
+export * from "./actions";
+export * from "./reducers";
+export * from "./selectors";
+
+export default { store };
---
+++ src/services/i18n/i18n.ts
@@ -0,0 +1,17 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "src/i18n/en.json";
+
+export const resources = {
+  en: { translation: en },
+} as const;
+
+export const init = async () =>
+  i18next.use(initReactI18next).init({
+    lng: "en",
+    debug: process.env.NODE_ENV === "development",
+    resources,
+  });
+
+export default { init };
